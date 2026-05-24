eb9e62acd042550d988c29293c37198ea084f1f2
Yuzhe Zhang <yuhzhang@uni-mainz.de>
2026-05-24 18:40:14 +0200

    added basics of ALP fields

--- scripts-and-data/axion_a_0.py
+++ scripts-and-data/axion_a_0.py
@@ -3,5 +3,8 @@ from src.constants import *
 
 rho_E_DM = 0.4 * unit.GeV / (unit.cm**3)
 m_a  = 1 * unit.ueV / const.c**2
-a_0:Quantity = np.sqrt(rho_E_DM * 2 * const.hbar **3 / (const.c * m_a **2))
-print(a_0.si)
+a_0_KimballOverview:Quantity = np.sqrt(rho_E_DM * 2 * const.hbar **3 / (const.c * m_a **2))
+a_0_Gramolin: Quantity =  const.hbar*np.sqrt(rho_E_DM * 2) / (const.c * m_a)
+
+print(a_0_KimballOverview.si)
+print(a_0_Gramolin.si)
