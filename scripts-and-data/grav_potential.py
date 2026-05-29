a82ce56e57f6d64c0cbc2869ff15f4bedc5e8a9b
Yuzhe Zhang <yuhzhang@uni-mainz.de>
2026-05-29 15:13:43 +0200

    modified for ALP field estimations

--- scripts-and-data/grav_potential.py
+++ scripts-and-data/grav_potential.py
@@ -1,10 +1,22 @@
 from src.dependency import *
 from src.constants import *
-from src.utils import check
+
 m_a = 1 * unit.ueV / const.c**2
 earth_pot = -const.G * unit.earthMass / unit.earthRad
-check(m_a.to(unit.kg))
-check(unit.earthMass.to(unit.kg))
-check(unit.earthRad.to(unit.km))
-check(earth_pot.to(unit.MJ / unit.kg))
-check(np.abs(earth_pot * 10 * unit.eV / const.c**2).to(unit.eV))
+earth_pot_10eV = np.abs(earth_pot * 10 * unit.eV / const.c**2).to(unit.eV)
+
+print("ALP mass m_a (1 ueV):", m_a.to(unit.kg))
+print("Earth mass M_E:", unit.earthMass.to(unit.kg))
+print("Earth radius R_E:", unit.earthRad.to(unit.km))
+print("Gravitational potential at surface G*M_E/R_E:", earth_pot.to(unit.MJ / unit.kg))
+print("Gravitational binding energy for 10 eV/c^2 ALP:", earth_pot_10eV)
+
+# escape velocity at Earth's surface: v_esc = sqrt(2 G M_E / R_E)
+v_esc = np.sqrt(2 * const.G * unit.earthMass / unit.earthRad)
+print("Escape velocity v_esc = sqrt(2 G M_E / R_E):", v_esc.to(unit.km / unit.s))
+
+# maximum kinetic energy for a bound ALP: E_k = (1/2) m_a v_esc^2 = G M_E m_a / R_E
+E_k_max = 0.5 * m_a * v_esc**2
+print("Max kinetic energy of bound ALP (m_a = 1 ueV/c^2):", E_k_max.to(unit.eV))
+
+v_a = (2 * earth_pot_10eV) / m_a
